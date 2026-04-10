2f0b73410b082bf452f4040f9722326bb710684b
anshul yadav <anshul.yadav@cvent.com>
2026-04-10 21:49:40 +0530

    initialize Spring Boot project

---
+++ src/main/java/com/abversion/experiment_service/ExperimentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.abversion.experiment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperimentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExperimentServiceApplication.class, args);
+	}
+
+}
